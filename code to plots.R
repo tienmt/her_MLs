879d851a9a94270ea0c59a92a4cc89174622e91b
The Tien Mai <39219912+tienmt@users.noreply.github.com>
2023-01-10 11:36:06 +0100

    clean codes to plots

--- code to plots.R
+++ code to plots.R
@@ -159,3 +159,90 @@ arrows(x+0.1, cau.low, x+0.1, cau.up, length=0.09, angle=90, code=3,
 axis(1, at=1:7, labels=c('Enet','Eprism','MLE','Moment','SLasso','GCTA','BoostHER'))
 
 
+
+Eprism[Eprism>1] = 1
+epr.boost[epr.boost>1] = 1
+Eprism[Eprism < 0] = 0
+epr.boost[epr.boost < 0] = 0
+
+boxplot(cbind(h2aprx,Enet,
+              Eprism,B_Eprism=epr.boost,
+              MLE=Re(MLE),B_MLE=mle.boost,
+              Moment,B_mm = mm.boost,
+              HERRA, B_herra = herra.boost),
+        main=bquote('screening 90%, 20 random SNPs,'~ sigma[epsilon]^2==1~","~ h^2==0.) )
+abline(h=mean(h2aprx))
+~","~ h^2==0.8
+1015x600
+
+
+
+boxplot(cbind(Chloramphenical= 1-unlist(herra.boost$y.chl)/var(phenotypes$y.chl),
+              Erythromycin= 1-unlist(herra.boost$y.eryth)/var(phenotypes$y.eryth),
+              Tetracycline= 1-unlist(herra.boost$y.tetracy)/var(phenotypes$y.tetracy),
+              penicillin= 1-unlist(herra.boost$ylactam)/var(phenotypes$ylactam),
+              'Co-trimoxazole'= 1-unlist(herra.boost$ytrime)/var(phenotypes$ytrime))
+        ,staplelwd = 2 , boxfill = NA, border = NA)
+boxplot(cbind(Chloramphenical= 1-unlist(herra.boost$y.chl)/var(phenotypes$y.chl),
+              Erythromycin= 1-unlist(herra.boost$y.eryth)/var(phenotypes$y.eryth),
+              Tetracycline= 1-unlist(herra.boost$y.tetracy)/var(phenotypes$y.tetracy),
+              penicillin= 1-unlist(herra.boost$ylactam)/var(phenotypes$ylactam),
+              'Co-trimoxazole'= 1-unlist(herra.boost$ytrime)/var(phenotypes$ytrime))
+        , xaxt = "n", add = TRUE,  boxwex=0.25, 
+        boxfill="black", at = 1:5 - 0.15)
+boxplot(cbind(Chloramphenical= zs.chl.b.herra,
+              Erythromycin= zs.eryth.b.herra,
+              Tetracycline= zs.tetra.b.herra,
+              penicillin= zs.oxacilliin.b.herra,
+              'Co-trimoxazole'= zs.trimtho.b.herra)
+        , xaxt = "n", add = TRUE,  boxwex=0.25,
+        boxfill="grey",  at = 1:5+ 0.15)
+legend("topright",inset=.01, cex = 1,
+       c("binary phenotypes","continuous zone sizes"),bg="grey96",
+       horiz=F, lty=c(5,5),lwd=c(30,30),
+       col = c("white","grey"))
+
+
+
+Eprism[Eprism>1] = 1
+epr.boost[epr.boost>1] = 1
+Eprism[Eprism < 0] = 0
+epr.boost[epr.boost < 0] = 0
+
+boxplot(cbind(h2aprx,Enet,
+              Eprism,B_Eprism=epr.boost,
+              MLE=Re(MLE),B_MLE=mle.boost,
+              Moment,B_mm = mm.boost,
+              HERRA, B_herra = herra.boost),
+        main=bquote('screening 90%, 20 random SNPs,'~ sigma[epsilon]^2==1~","~ h^2==0.) )
+abline(h=mean(h2aprx),col='red')
+~","~ h^2==0.8
+1015x600
+
+
+boxplot(cbind(Chloramphenicol= 1-unlist(herra.boost$y.chl)/var(phenotypes$y.chl),
+              Erythromycin= 1-unlist(herra.boost$y.eryth)/var(phenotypes$y.eryth),
+              Tetracycline= 1-unlist(herra.boost$y.tetracy)/var(phenotypes$y.tetracy),
+              Penicillin= 1-unlist(herra.boost$ylactam)/var(phenotypes$ylactam),
+              'Co-trimoxazole'= 1-unlist(herra.boost$ytrime)/var(phenotypes$ytrime))
+        ,staplelwd = 2 , boxfill = NA, border = NA)
+boxplot(cbind(Chloramphenicol= 1-unlist(herra.boost$y.chl)/var(phenotypes$y.chl),
+              Erythromycin= 1-unlist(herra.boost$y.eryth)/var(phenotypes$y.eryth),
+              Tetracycline= 1-unlist(herra.boost$y.tetracy)/var(phenotypes$y.tetracy),
+              Penicillin= 1-unlist(herra.boost$ylactam)/var(phenotypes$ylactam),
+              'Co-trimoxazole'= 1-unlist(herra.boost$ytrime)/var(phenotypes$ytrime))
+        , xaxt = "n", add = TRUE,  boxwex=0.25, 
+        boxfill="red", at = 1:5 - 0.15)
+boxplot(cbind(Chloramphenicol= zs.chl.b.herra,
+              Erythromycin= zs.eryth.b.herra,
+              Tetracycline= zs.tetra.b.herra,
+              Penicillin= zs.oxacilliin.b.herra,
+              'Co-trimoxazole'= zs.trimtho.b.herra)
+        , xaxt = "n", add = TRUE,  boxwex=0.25,
+        boxfill="green",  at = 1:5+ 0.15)
+legend("topright",inset=.01, cex = 1,
+       c("binary phenotypes","continuous zone sizes"),bg="grey96",
+       horiz=F, lty=c(1,1),lwd=c(10,10),
+       col = c("red","green"))
+
+
